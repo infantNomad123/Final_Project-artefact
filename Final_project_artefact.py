842bccd4da8b5b8370ce2d7a9a2151d2285eb1ff
Mr-roachz <47786872+Mr-roachz@users.noreply.github.com>
2020-05-18 02:19:30 +0100

    Update Final_project_artefact.py

--- Final_project_artefact.py
+++ Final_project_artefact.py
@@ -1,9 +1,11 @@
 import random
+#Sets mode
 ENC =0
 DEC =1
 
-def parseKey(key):
-
+#creates key
+#The key values are constituted with numbers rather than alphabet
+def parseKey(key):        
     tmp = []
  
     for i, k in enumerate(str(key)):
@@ -20,29 +22,39 @@ def parseKey(key):
 
     return enc_table, dec_table
 
+#Transposes values
+#Makes encryption and decryption tables
 def transposition(msg, key, mode):
 
+    #Sets the length of the plaintext message
     msgsize = len(msg)
+    
+    #Sets the length of the key
     keysize = len(str(key))
     ret = ''
 
     filler = ''
+    #Fills any left out space if the length of the message is not the multiple of keysize
     if msgsize%keysize != 0:
         code = ['5', '2', '#', '8', '1', '3','4', '6', '0', '9', '*', '%', '=', '(', ')']
         random_code = random.choice(code)
         filler = '0' * (keysize - msgsize%keysize)
         
-
+    #Sets the texts to an upper case and fills out the space
     msg = msg.upper()
     msg+=filler
-
+    
+    #calls encryption and decryption table from the function parsekey(key)
     enc_table, dec_table = parseKey(key)
-
+    
+    #Decides the action to be taken on the basis of mode - ENC ord DEC
+    #If it is in ENC mode call the encryption table
     if mode == ENC:
         table = enc_table
+    #If not call the decryption table
     else:
         table = dec_table
-
+    #Encrypts the text
     if mode == ENC:
         buf = [''] * keysize
         for i,c in enumerate(msg):
@@ -52,7 +64,8 @@ def transposition(msg, key, mode):
 
         for text in buf:
             ret += text
-
+            
+    #Decrypts the text
     else:
         blocksize = int(msgsize/keysize)
         buf = ['']*keysize
@@ -69,14 +82,20 @@ def transposition(msg, key, mode):
                     ret += buf[j][i]
 
     return ret
+#Displays the result
 def main():
+    #Sets the key value
     key = 12012
+    
+    #Sets the message to encrypted
     msg = "TREASURE BOX IS BURRIED AT TWO HUNDRED FEET TO NORTH EAST AWAY FROM YOUR HOME"
     print('Original:%s' %msg.upper())
-
+    
+    #Displays encryted message
     ciphertext = transposition(msg, key, ENC)
     print('Ciphered:%s' %ciphertext)
-
+    
+    #Displays decrypted message
     deciphertext = transposition(ciphertext, key, DEC)
     print('Deciphered:%s' %deciphertext)
     
